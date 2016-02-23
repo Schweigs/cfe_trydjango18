bd37324edecca68320e5c48500672a9a00060070
Schweigs <mjschweiger@gmail.com>
2016-02-23 20:51:17 +0000

    Lecture 56: View & Template Context

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, "home.html", {})
+    if request.user.is_authenticated():
+        title = 'My Title %s' %(request.user)
+    else:
+        title = 'Welcome'
+    context = {
+        'title': title,
+    }
+    return render(request, "home.html", context)
