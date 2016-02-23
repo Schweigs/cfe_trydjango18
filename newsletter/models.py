b3971bc9878d6f34e334f0e38384d0c9b81b4de5
Schweigs <mjschweiger@gmail.com>
2016-02-23 20:12:31 +0000

    Lecture 53: Admin

--- newsletter/models.py
+++ newsletter/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class SignUp(models.Model):
     email = models.EmailField()
-    full_name = models.CharField(default='', blank=True, null=True, max_length=120)
+    full_name = models.CharField(default='', blank=False, null=True, max_length=120)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
