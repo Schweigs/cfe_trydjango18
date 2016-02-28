1c49acdc8f6e3e1783c491fd814eedf16d47233c
Schweigs <mjschweiger@gmail.com>
2016-02-28 17:43:42 +0000

    Lecture 26: Styling MVP Landing Part2

--- newsletter/views.py
+++ newsletter/views.py
@@ -36,13 +36,13 @@ def home(request):
 
 
 def contact(request):
+    title = 'Contact Us'
+    title_align_center = False
     form = ContactForm(request.POST or None)
     if form.is_valid():
-
         # for key in form.cleaned_data:
         #     print key
         #     print form.cleaned_data.get(key)
-
         form_full_name = form.cleaned_data.get('full_name')
         form_email = form.cleaned_data.get('email')
         form_message = form.cleaned_data.get('message')
@@ -50,13 +50,14 @@ def contact(request):
         # print form_email
         # print form_message
         subject = 'Site contact form'
-
         from_email = settings.EMAIL_HOST_USER
         to_email = [from_email, 'yourotheremail@email.com']
         contact_message = '%s: %s via %s' %(form_full_name, form_message, form_email)
         send_mail(subject, contact_message, from_email, [to_email], fail_silently=False)
     context = {
         'form': form,
+        'title': title,
+        'title_align_center': title_align_center,
     }
 
     return render(request, "forms.html", context)
